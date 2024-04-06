d8a36382ebe9bb053fa5158cd3881625427f2ec2
Saleem Meer <smeer@pcon.as>
2024-04-06 19:55:42 +0200

    first commit

---
+++ Google Vision OCR WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Google_Vision_OCR_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Google Vision OCR WPF/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Google.Cloud.Vision.V1;
+using System.Windows;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
+
+namespace Google_Vision_OCR_WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var fileNames = openFileDialog.FileNames;
+                foreach (string file in fileNames)
+                {
+                    txtFilePath.Text = file;
+                    imgPageImage.Source = new BitmapImage(new Uri( txtFilePath.Text));
+                }
+
+            }
+        }
+
+        private void butOcrStart_Click(object sender, RoutedEventArgs e)
+        {
+            doOcr(txtFilePath.Text);
+        }
+
+        private void doOcr(string fileName)
+        {
+            var jsonPath = @"E:\temp\ocr-img\urdu-text-ocr-80e9e1c09809.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", jsonPath);
+
+            var client = ImageAnnotatorClient.Create();
+            var image = Image.FromFile(fileName);
+
+            try
+            {
+                TextAnnotation response = client.DetectDocumentText(image);
+                rtbUrduOcrText.Document.Blocks.Clear();
+                rtbUrduOcrText.AppendText(response.Text);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+    }
+}
